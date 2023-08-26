33f08b0f9eeacc57b0243b1d09c5d02c3d9cf830
mvfflin <mynameisfatihul@gmail.com>
2023-08-26 22:42:34 +0700

    minor update

--- controllers/loginControl.ts
+++ controllers/loginControl.ts
@@ -1,48 +1,48 @@
 import { Request, Response } from "express";
 import { user } from "../schema/user";
 import { compareSync } from "bcrypt";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const loginControl = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    try {
-        if (!username || !password) {
-            return res
-                .status(201)
+  try {
+    if (!username || !password) {
+      return res
+        .status(201)
 
-                .json({ error: "Incomplete credentials" });
-        }
-
-        const findUser = await user.findOne({
-            username: username,
-        });
-
-        if (!findUser) {
-            return res
-                .status(202)
-
-                .json({ error: "User not found" });
-        } else {
-            const thePass = findUser.password as string;
-            const checkPass = compareSync(password, thePass);
-
-            if (!checkPass) {
-                return res.status(203).json({ error: "Incorrect password" });
-            } else {
-                const jwtToken = jwt.sign(
-                    {
-                        id: findUser.id,
-                        username: findUser.username,
-                        email: findUser.email,
-                    },
-                    process.env.JWTKEY!
-                );
-                return res.status(200).json({ findUser, token: jwtToken });
-            }
-        }
-    } catch (err) {
-        console.log(`smth wrong : ${err}`);
-        return res.status(204).json({ error: "Unknown error." });
+        .json({ error: "Incomplete credentials" });
     }
+
+    const findUser = await user.findOne({
+      username: username,
+    });
+
+    if (!findUser) {
+      return res
+        .status(202)
+
+        .json({ error: "User not found" });
+    } else {
+      const thePass = findUser.password as string;
+      const checkPass = compareSync(password, thePass);
+
+      if (!checkPass) {
+        return res.status(203).json({ error: "Incorrect password" });
+      } else {
+        const jwtToken = jwt.sign(
+          {
+            id: findUser.id,
+            username: findUser.username,
+            email: findUser.email,
+          },
+          process.env.JWTKEY!
+        );
+        return res.status(200).json({ findUser, token: jwtToken });
+      }
+    }
+  } catch (err) {
+    console.log(`smth wrong : ${err}`);
+    return res.status(204).json({ error: "Unknown error." });
+  }
 };
--- index.ts
+++ index.ts
@@ -4,37 +4,59 @@ import mongoose from "mongoose";
 import authRouter from "./routes/authRoutes";
 import { userRouter } from "./routes/userRoutes";
 import { accountRouter } from "./routes/accountRoutes";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
 import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 
+const server = createServer(app);
+app.use(cors());
+const io: Server = new Server(server, {
+  cors: {},
+});
+io.on("connection", (socket) => {
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
 var mt: boolean = false;
 
 var mongodb: string = process.env.MONGODB!;
 
 async function checkMongo() {
-    try {
-        await mongoose.connect(mongodb, {
-            dbName: "muffinforum",
-        });
-        console.log("connected to db");
-        mt = false;
-    } catch (err) {
-        console.log("database not connected");
-        console.log("the error : " + err);
-        mt = true;
-    }
+  try {
+    await mongoose.connect(mongodb, {
+      dbName: "muffinforum",
+    });
+    console.log("connected to db");
+    mt = false;
+  } catch (err) {
+    console.log("database not connected");
+    console.log("the error : " + err);
+    mt = true;
+  }
 }
 checkMongo();
-setTimeout(checkMongo, 10000);
+setInterval(checkMongo, 10000);
 
-app.listen(1231, () => {
-    console.log(`server api online at : http://localhost:1231`);
+server.listen(1231, () => {
+  console.log(`server api online at : http://localhost:1231`);
 });
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth/", authRouter);
@@ -42,10 +64,10 @@ app.use("/api/users/", userRouter);
 app.use("/api/account/", accountRouter);
 
 app.get("/", (req: Request, res: Response) => {
-    console.log("api pinged");
-    if (mt == true) {
-        res.status(201).send("server maintenance cuz not connected to db");
-    } else {
-        res.status(200).send("server running");
-    }
+  console.log("api pinged");
+  if (mt == true) {
+    res.status(201).send("server maintenance cuz not connected to db");
+  } else {
+    res.status(200).send("server running");
+  }
 });
